cee45bf2444b64c17d54910d5a90dc52043ce722
PoolVara <polvr17@gmail.com>
2024-09-23 12:12:17 +0200

    23/09/2024

---
+++ javascript/ej3/main.js
@@ -0,0 +1,34 @@
+let nombre = ('Pol')
+console.log (nombre)
+console.log (typeof nombre)
+
+let edad = (21)
+console.log (edad)
+console.log (typeof edad)
+
+let boolean = (false)
+console.log (boolean)
+console.log (typeof boolean)
+
+let nada = null
+console.log (nada)
+console.log (typeof nada)
+
+let sindefinir = undefined
+console.log (sindefinir)
+console.log(typeof sindefinir)
+
+let simbolo = Symbol('Cosa')
+console.log(simbolo)
+console.log (typeof simbolo)
+
+let array= [1, 'texto', false]
+console.log (array)
+console.log (typeof array)
+
+let funcion = function(){
+    console.log (funcion)
+    console.log (typeof funcion)
+
+}
+    
